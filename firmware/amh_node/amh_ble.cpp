41ae20a624ad7b33636a9a0a8fc920cdc99908f2
Jobin Babu Ayathil <jobinb6444@gmail.com>
2026-06-22 22:32:26 +0530

    Add BLE telemetry service

---
+++ firmware/amh_node/amh_ble.cpp
@@ -0,0 +1,43 @@
+#include <bluefruit.h>
+#include "amh_ble.h"
+
+static uint8_t SERVICE_UUID[16] = {
+  0x66,0x66,0x55,0x55,0x44,0x44,0x33,0x33,
+  0x22,0x22,0x11,0x11,0x01,0x00,0x00,0xf0
+};
+static uint8_t CHAR_UUID[16] = {
+  0x66,0x66,0x55,0x55,0x44,0x44,0x33,0x33,
+  0x22,0x22,0x11,0x11,0x02,0x00,0x00,0xf0
+};
+
+static BLEService amhService(SERVICE_UUID);
+static BLECharacteristic amhTelemetry(CHAR_UUID);
+
+void ble_init(int tx_power) {
+  Bluefruit.begin();
+  Bluefruit.setTxPower(tx_power);
+  Bluefruit.setName("AMH-Node");
+
+  amhService.begin();
+  amhTelemetry.setProperties(CHR_PROPS_NOTIFY);
+  amhTelemetry.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  amhTelemetry.setFixedLen(8);
+  amhTelemetry.begin();
+
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addService(amhService);
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);
+  Bluefruit.Advertising.setFastTimeout(30);
+  Bluefruit.Advertising.start(0);
+}
+
+void ble_notify(float drift_variance, float battery) {
+  if (!Bluefruit.connected()) return;
+  float payload[2] = { drift_variance, battery };
+  amhTelemetry.notify((uint8_t *)payload, sizeof(payload));
+}
+
+bool ble_connected() { return Bluefruit.connected(); }
---
+++ firmware/amh_node/amh_ble.h
@@ -0,0 +1,11 @@
+#ifndef AMH_BLE_H
+#define AMH_BLE_H
+
+void ble_init(int tx_power);
+
+// Notifies an 8-byte payload: float32 drift_variance, float32 battery (LE).
+void ble_notify(float drift_variance, float battery);
+
+bool ble_connected();
+
+#endif // AMH_BLE_H
